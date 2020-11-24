29bbd2a719277418c61afc7b20251bce669dbd87
Gabriel Sena <gabesenacom@gmail.com>
2020-11-23 23:39:45 -0300

    Add substrings project

---
+++ substrings/substrings.rb
@@ -0,0 +1,21 @@
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+def substrings(string, dictionary)
+  dictionary.reduce({}) do | words, word |
+    found = string.downcase.scan(/#{word}/).size
+    words[word] = found if found > 0
+    words
+  end
+end
+
+
+p substrings("below", dictionary)
+# {"below"=>1, "low"=>1}
+p substrings("be?low!", dictionary)
+# {"low"=>1}
+p substrings("below!", dictionary)
+# {"below"=>1, "low"=>1}
+p substrings("sit it sit sit i", dictionary)
+# {"it"=>4, "i"=>5, "sit"=>3}
+p substrings("Howdy partner, sit down! How's it going?", dictionary)
+# {"down"=>1, "go"=>1, "going"=>1, "how"=>2, "howdy"=>1, "it"=>2, "i"=>3, "own"=>1, "part"=>1, "partner"=>1, "sit"=>1}
